920296997cc14e600051463ed3a34a66c8a9966c
«Antonina <a1523@mail.ru>
2022-09-19 13:31:16 +0300

    Add Task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет 
+количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2
+*/
+
+int[] GetArray()
+{
+    int[] array = new int[3];
+    for(int i=0; i<3; i++)
+    {
+        array[i] = new Random().Next(1, 1000);
+    }
+    return array;
+}
+
+int PositiveCount(int[] array)
+{
+    int count = 0;
+    for(int i=0; i<array.Length; i++)
+    {
+        if (array[i]%2 == 0)
+        {
+            count = count + 1;
+        }
+    }
+    return count;
+}
+
+void PrintArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    return;
+}
+
+int[] array = GetArray();
+
+Console.WriteLine("Задан массив: ");
+PrintArray(array);
+Console.WriteLine();
+
+int count = PositiveCount(array);
+Console.WriteLine($"Количество четных чисел в массиве = {count}");
